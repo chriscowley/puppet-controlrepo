976a182711bc02dd40cfeea943dc469eecaed0f7
Chris Cowley <chris@chriscowley.me.uk>
2016-05-07 21:52:26 +0200

    Collect SSL vhosts from Hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -212,10 +212,13 @@ node default {
     'web': {
       $webvhosts = hiera('apache::vhosts', {})
       create_resources('apache::vhost', $webvhosts)
-      letsencrypt::certonly { 'mirror.chriscowley.me.uk':
-        plugin => 'webroot',
-        webroot_paths => [ '/var/www/mirror.chriscowley.me.uk/' ]
-      }
+      $letsencryptcerts = hiera('letsencrypt::certonly', {})
+      create_resources('letsencrypt::certonly', $letsencryptcerts)
+      #      letsencrypt::certonly { 'mirror.chriscowley.me.uk':
+      #  plugin => 'webroot',
+      #   webroot_paths => [ '/var/www/mirror.chriscowley.me.uk/' ]
+      #}
+      hiera_resources('apache-ssl-vhosts')
     }
     default: {
     }
